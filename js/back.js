74d90f75a91e791412ac0bf31d628ac82876b514
ming <minghengccit@126.com>
2020-12-04 17:23:59 +0800

    xxx

--- js/back.js
+++ js/back.js
@@ -1,5 +1,4 @@
 setInterval(function(){
      
-    $("span:contains('1.38')").click();
     console.log(1)
         }, 2000);
--- popup.js
+++ popup.js
@@ -1,11 +1,3 @@
 $("#btn").click(function(){
     alert($("#he").text());
 });
-
-
-
-$("#ttt").click(function(){
-    var s = $("span:contains('3.0')");
-    s.click();
-    alert(s.html());
-});
